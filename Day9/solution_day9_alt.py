8708d61fef623b02f8e3c3b06eedab2b13a3c618
campsight <ir.j.camp@gmail.com>
2019-12-25 11:26:50 +0100

    solutions day7 implemented

---
+++ Day9/solution_day9_alt.py
@@ -0,0 +1,134 @@
+def extract_modes(instruction):
+    instruction_set = [int(x[0]) for x in instruction]
+    #print(instruction_set)
+    n = len(instruction_set)
+    if (n==1):
+        opcode = instruction_set.pop()
+        return [opcode]
+    else:
+        opcode = instruction_set.pop() + instruction_set.pop()*10
+        instruction_set.reverse()
+        return ([opcode] + instruction_set)
+
+# helper function to make process_step more readable
+# 'solves' the instruction mode (value vs address) topic
+def get_instruction_values(my_list, instruction_pointer, instruction_mode, nb_of_params = 1, relative_base = 0):
+    while (len(instruction_mode) < (nb_of_params+1)): instruction_mode.append(0)
+    values = []
+    for i in range(nb_of_params):
+        param = my_list[instruction_pointer + i + 1]
+        # 2 = relative mode: offset to relative position
+        if instruction_mode[i+1] == 0: # position mode: param is value at the address
+            values.append(my_list[param])
+        elif instruction_mode[i+1] == 2: # relative mode: offset to relative position
+            #print("relative mode: ", param, relative_base)
+            values.append(my_list[param + relative_base])
+        else: # immediate mode: param is value itself
+            values.append(param)
+    return values
+
+def get_store_pos(instruction_mode, nb_of_params, relative_base, normal_store_pos):
+    while (len(instruction_mode) < (nb_of_params+1)): instruction_mode.append(0)
+    mode = instruction_mode.pop()
+    if mode == 2: normal_store_pos += relative_base
+    return normal_store_pos
+
+
+# AoC 2019 processor. Input: a program (my_list), an instruction_pointer (pointing to next instruction), the inputs, outputs and haltcode
+# haltcode = 1 to continue, 0 to wait for input and 99 if the program halts
+def process_step(my_list, instruction_pointer, input=[], output=[], haltcode=1, relative_base = 0):
+    if (haltcode == 0): return (my_list, instruction_pointer, input, output, haltcode, relative_base)
+    if (haltcode == 99): return (my_list, instruction_pointer, input, output, haltcode, relative_base)
+    #print(instruction_pointer, output, my_list)
+    instruction = my_list[instruction_pointer]
+    instruction_mode = extract_modes(str(instruction))
+    opcode = instruction_mode[0]
+    if (opcode == 1):
+        values = get_instruction_values(my_list, instruction_pointer, instruction_mode, 2, relative_base)
+        store_pos = get_store_pos(instruction_mode, 3, relative_base, my_list[instruction_pointer+3])
+        my_list[store_pos] = values[0] + values[1]
+        #return process_step(my_list, instruction_pointer + 4, input, output, 1, relative_base)
+        return (my_list, instruction_pointer + 4, input, output, 1, relative_base)
+    elif (opcode == 2):
+        values = get_instruction_values(my_list, instruction_pointer, instruction_mode, 2, relative_base)
+        store_pos = get_store_pos(instruction_mode, 3, relative_base, my_list[instruction_pointer+3])
+        my_list[store_pos] = values[0] * values[1]
+        #return process_step(my_list, instruction_pointer + 4, input, output, 1, relative_base)
+        return (my_list, instruction_pointer + 4, input, output, 1, relative_base)
+    elif (opcode == 3): #read input - but could be that it needs to wait if there is non
+        store_pos = get_store_pos(instruction_mode, 1, relative_base, my_list[instruction_pointer+1])
+        if (len(input) > 0):
+            my_list[store_pos] = input.pop(0)
+            #return process_step(my_list, instruction_pointer + 2, input, output, 1, relative_base)
+            return (my_list, instruction_pointer + 2, input, output, 1, relative_base)
+        else: #no input anymore => halt and don't move the instruction pointer
+            #return process_step(my_list, instruction_pointer, input, output, 0, relative_base)
+            return (my_list, instruction_pointer, input, output, 0, relative_base)
+    elif (opcode == 4): #output
+        values = get_instruction_values(my_list, instruction_pointer, instruction_mode, 1, relative_base)
+        output.append(values[0])
+        #return process_step(my_list, instruction_pointer + 2, input, output, 1, relative_base)
+        return (my_list, instruction_pointer + 2, input, output, 1, relative_base)
+    elif ((opcode == 5) or (opcode == 6)): #jump-if true(5) or false(6)
+        values = get_instruction_values(my_list, instruction_pointer, instruction_mode, 2, relative_base)
+        cond = values[0]
+        if (((opcode == 5) and (cond != 0)) or ((opcode == 6) and (cond ==0))):
+            #return process_step(my_list, values[1], input, output, 1, relative_base)
+            return (my_list, values[1], input, output, 1, relative_base)
+        else:
+            #return process_step(my_list, instruction_pointer + 3, input, output, 1, relative_base)
+            return (my_list, instruction_pointer + 3, input, output, 1, relative_base)
+    elif ((opcode == 7) or (opcode == 8)):
+        values = get_instruction_values(my_list, instruction_pointer, instruction_mode, 2, relative_base)
+        store_pos = get_store_pos(instruction_mode, 3, relative_base, my_list[instruction_pointer+3])
+        if (((opcode == 7) and (values[0] < values[1])) or ((opcode==8) and (values[0] == values[1]))):
+            my_list[store_pos] = 1
+        else:
+            my_list[store_pos] = 0
+        #return process_step(my_list, instruction_pointer + 4, input, output, 1, relative_base)
+        return (my_list, instruction_pointer + 4, input, output, 1, relative_base)
+    elif (opcode == 9):
+        values = get_instruction_values(my_list, instruction_pointer, instruction_mode, 1, relative_base)
+        relative_base += values[0]
+        #return process_step(my_list, instruction_pointer + 2, input, output, 1, relative_base)
+        return (my_list, instruction_pointer + 2, input, output, 1, relative_base)
+    elif (opcode == 99):
+        # print("finished", my_list)
+        #return (my_list, output, instruction_pointer, input, 99)
+        return (my_list, instruction_pointer, input, output, 99, relative_base)
+    else:  # something went wrong
+        print("Error, command unkonwn: ")
+        # print(instruction_pointer)
+        # print(my_list)
+        #return process_step(my_list, instruction_pointer + 2, input, output, -1, relative_base)
+        return (my_list, instruction_pointer, input, output, -1, relative_base)
+
+def run_program(instructions, input):
+    instruction_pointer = 0
+    output = []
+    relative_base = 0
+    halt_code = 1
+    finished = False
+    while not finished:
+        result = process_step(instructions, instruction_pointer, input, output, halt_code, relative_base)
+        instructions = result[0]
+        instruction_pointer = result[1]
+        input = result[2]
+        output = result[3]
+        halt_code = result[4]
+        relative_base = result[5]
+        if (halt_code != 1):
+            print("halting because of haltcode ", halt_code)
+            finished = True
+    return output
+
+instructions = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+instructions = [1102,34915192,34915192,7,4,7,99,0]
+instructions = [104,1125899906842624,99]
+instructions = [1102,34463338,34463338,63,1007,63,34463338,63,1005,63,53,1102,1,3,1000,109,988,209,12,9,1000,209,6,209,3,203,0,1008,1000,1,63,1005,63,65,1008,1000,2,63,1005,63,904,1008,1000,0,63,1005,63,58,4,25,104,0,99,4,0,104,0,99,4,17,104,0,99,0,0,1102,1,21,1004,1101,28,0,1016,1101,0,27,1010,1102,36,1,1008,1102,33,1,1013,1101,0,22,1012,1101,0,37,1011,1102,34,1,1017,1102,466,1,1027,1102,1,484,1029,1102,1,699,1024,1102,1,1,1021,1101,0,0,1020,1102,1,24,1015,1101,0,473,1026,1101,653,0,1022,1102,26,1,1007,1102,25,1,1006,1101,0,39,1014,1102,646,1,1023,1101,690,0,1025,1102,1,29,1019,1101,32,0,1018,1101,30,0,1002,1101,0,20,1001,1102,1,38,1005,1102,1,23,1003,1101,0,31,1000,1101,35,0,1009,1101,0,493,1028,109,5,1208,0,37,63,1005,63,201,1001,64,1,64,1106,0,203,4,187,1002,64,2,64,109,-4,2107,36,8,63,1005,63,223,1001,64,1,64,1105,1,225,4,209,1002,64,2,64,109,18,21107,40,41,-9,1005,1010,243,4,231,1105,1,247,1001,64,1,64,1002,64,2,64,109,6,21107,41,40,-9,1005,1016,267,1001,64,1,64,1106,0,269,4,253,1002,64,2,64,109,-19,21102,42,1,5,1008,1011,42,63,1005,63,291,4,275,1105,1,295,1001,64,1,64,1002,64,2,64,109,15,1205,0,309,4,301,1105,1,313,1001,64,1,64,1002,64,2,64,109,-27,2101,0,9,63,1008,63,20,63,1005,63,333,1106,0,339,4,319,1001,64,1,64,1002,64,2,64,109,19,21102,43,1,6,1008,1019,45,63,1005,63,363,1001,64,1,64,1105,1,365,4,345,1002,64,2,64,109,1,21108,44,47,-3,1005,1011,385,1001,64,1,64,1106,0,387,4,371,1002,64,2,64,109,-22,1201,9,0,63,1008,63,21,63,1005,63,411,1001,64,1,64,1106,0,413,4,393,1002,64,2,64,109,9,1207,0,19,63,1005,63,433,1001,64,1,64,1106,0,435,4,419,1002,64,2,64,109,-9,2107,30,8,63,1005,63,453,4,441,1105,1,457,1001,64,1,64,1002,64,2,64,109,25,2106,0,10,1001,64,1,64,1106,0,475,4,463,1002,64,2,64,109,11,2106,0,0,4,481,1001,64,1,64,1105,1,493,1002,64,2,64,109,-18,2108,21,-6,63,1005,63,511,4,499,1106,0,515,1001,64,1,64,1002,64,2,64,109,-12,2108,18,6,63,1005,63,535,1001,64,1,64,1106,0,537,4,521,1002,64,2,64,109,19,21101,45,0,-7,1008,1010,45,63,1005,63,563,4,543,1001,64,1,64,1105,1,563,1002,64,2,64,109,-10,1207,-5,31,63,1005,63,581,4,569,1106,0,585,1001,64,1,64,1002,64,2,64,109,-8,2102,1,5,63,1008,63,21,63,1005,63,611,4,591,1001,64,1,64,1105,1,611,1002,64,2,64,109,5,1201,0,0,63,1008,63,21,63,1005,63,633,4,617,1106,0,637,1001,64,1,64,1002,64,2,64,109,13,2105,1,6,1001,64,1,64,1106,0,655,4,643,1002,64,2,64,109,-7,1202,-3,1,63,1008,63,26,63,1005,63,681,4,661,1001,64,1,64,1106,0,681,1002,64,2,64,109,12,2105,1,2,4,687,1001,64,1,64,1105,1,699,1002,64,2,64,109,-28,1208,8,30,63,1005,63,717,4,705,1106,0,721,1001,64,1,64,1002,64,2,64,109,10,1202,1,1,63,1008,63,40,63,1005,63,745,1001,64,1,64,1105,1,747,4,727,1002,64,2,64,109,10,21108,46,46,-2,1005,1012,765,4,753,1105,1,769,1001,64,1,64,1002,64,2,64,109,-2,1205,8,781,1106,0,787,4,775,1001,64,1,64,1002,64,2,64,109,-9,2101,0,0,63,1008,63,23,63,1005,63,809,4,793,1105,1,813,1001,64,1,64,1002,64,2,64,109,9,1206,8,831,4,819,1001,64,1,64,1106,0,831,1002,64,2,64,109,-9,2102,1,-2,63,1008,63,22,63,1005,63,855,1001,64,1,64,1106,0,857,4,837,1002,64,2,64,109,4,21101,47,0,10,1008,1017,50,63,1005,63,877,1105,1,883,4,863,1001,64,1,64,1002,64,2,64,109,18,1206,-4,895,1105,1,901,4,889,1001,64,1,64,4,64,99,21101,0,27,1,21102,915,1,0,1106,0,922,21201,1,56639,1,204,1,99,109,3,1207,-2,3,63,1005,63,964,21201,-2,-1,1,21102,1,942,0,1106,0,922,22102,1,1,-1,21201,-2,-3,1,21101,0,957,0,1106,0,922,22201,1,-1,-2,1106,0,968,22102,1,-2,-2,109,-3,2106,0,0]
+instructions = instructions + ([0]*100000)
+
+#result = process_step(instructions, 0, [1], [], 1, 0)
+result = run_program(instructions, [2])
+
+print(result)
